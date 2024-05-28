bba66258e8783cc57bbb2b434edbaf5c7ab34a47
fcursino <fabriciocursino09@gmail.com>
2024-05-27 22:19:06 -0300

    feat: changing image of logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,5 +1,5 @@
 import { LogoContainer, LogoImage } from "./style"
-import logo from '../../assets/logo.svg'
+import logo from '../../assets/memorinn.svg'
 
 interface LogoProps {
   children?: React.ReactNode
