23c412906a58657b7e931905906094ac8fd5743f
fcursino <fabriciocursino09@gmail.com>
2024-06-02 15:52:47 -0300

    create separated folder to keyframes

--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -1,16 +1,7 @@
-import { styled, keyframes } from "styled-components";
+import { styled } from "styled-components";
 import { latoBold } from "../../assets/fonts/Lato/lato";
+import { fadeInUp } from "../../assets/animations/fadeInUp";
 
-const fadeInUp = keyframes`
-    from {
-        opacity: 0;
-        transform: translateY(20px);
-    }
-    to {
-        opacity: 1;
-        transform: translateY(0);
-    }
-`;
 
 export const HomeBottomContainer = styled.div`
   display: flex;
