11faecf2cbab9af4111c047472ebe1cd6bd7cfaa
fcursino <fabriciocursino09@gmail.com>
2024-06-03 22:19:47 -0300

    organizing created components on the details page

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,14 +1,26 @@
-import { DetailsContainer, DetailsLeftContainer, DetailsRightContainer } from "./style"
+import Logo from "../../components/Logo"
+import { DetailsBottomContainer, DetailsContainer, DetailsContentContainer, DetailsLeftContainer, DetailsLogoTitle, DetailsNoCommentsMessage, DetailsRightContainer } from "./style"
 
 function Details () {
   return (
     <DetailsContainer>
-      <DetailsLeftContainer>
-
-      </DetailsLeftContainer>
-      <DetailsRightContainer>
-        
-      </DetailsRightContainer>
+      <Logo>
+        <DetailsLogoTitle>
+          MemorInn
+        </DetailsLogoTitle>
+      </Logo>
+      <DetailsBottomContainer>
+        <DetailsLeftContainer>
+          <DetailsContentContainer>
+          <p>Lorem ipsum dolor sit amet.r sit amet.sdfsadfs asfasrase serwrwe4 eraserwe we  werwerr. Lorem ipsum dolor sit amet.r sit amet.sdfsadfs asfasrase serwrwe4 eraserwe we  werwerr. Lorem ipsum dolor sit amet.r sit amet.sdfsadfs asfasrase serwrwe4 eraserwe we  werwerr. Lorem ipsum dolor sit amet.r sit amet.sdfsadfs asfasrase serwrwe4 eraserwe we  werwerr. Lorem ipsum dolor sit amet.r sit amet.sdfsadfs asfasrase serwrwe4 eraserwe we  werwerr. </p>
+          </DetailsContentContainer>
+        </DetailsLeftContainer>
+        <DetailsRightContainer>
+          <DetailsNoCommentsMessage>
+            Ainda não temos nenhum comentário sobre este livro
+          </DetailsNoCommentsMessage> 
+        </DetailsRightContainer>
+      </DetailsBottomContainer>
     </DetailsContainer>
   )
 }
