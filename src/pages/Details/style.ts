d4ff94c1a7baa85230ca10812f67ac1d06c1a615
fcursino <fabriciocursino09@gmail.com>
2024-06-03 22:19:25 -0300

    creating details page components of logo, content and comments

--- src/pages/Details/style.ts
+++ src/pages/Details/style.ts
@@ -1,16 +1,53 @@
 import styled from "styled-components";
+import { latoBold } from "../../assets/fonts/Lato/lato";
+
+export const DetailsBottomContainer = styled.div`
+  display: flex;
+`
 
 export const DetailsContainer = styled.div`
-  margin: 10px 30px;
+    margin: 10px 30px;
 `
 
 export const DetailsLeftContainer = styled.div`
   width: 65%;
-  margin: auto;
-  position: relative;
 `
 
 export const DetailsRightContainer = styled.div`
   width: 35%;
-  margin: 50px;
+`
+
+export const DetailsContentContainer = styled.div`
+    margin: 30px 10px;
+    max-width: 80%;
+    text-align: justify;
+    background-color: #F7F0E8;
+    padding: 10px;
+    border-radius: 10px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
+    display: block;
+    transform: scale(1);
+    transition: all .3s ease-in-out;
+    cursor: pointer;
+
+    &:hover {
+        transform: scale(1.02);
+    }
+`
+
+export const DetailsLogoTitle = styled.text`
+  ${latoBold};
+  font-size: 22px;
+  font-family: 'Lato', sans-serif;
+  color: #B02D2A;
+  font-weight: 700;
+`
+
+export const DetailsNoCommentsMessage = styled.text`
+    ${latoBold};
+    color: rgba(21, 19, 19, 0.15);
+    font-size: 37px;
+    font-family: 'Lato', sans-serif;
+    position: relative;
+    font-weight: 600;
 `
