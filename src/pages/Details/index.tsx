da6a7941a9d2385a5d7fcaaf3abe8b893d2a1036
fcursino <fabriciocursino09@gmail.com>
2024-06-09 16:22:03 -0300

    fetching book summary via gemini ai

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react"
 import Logo from "../../components/Logo"
 import { DetailsBottomContainer, DetailsContainer, DetailsContentContainer, DetailsLeftContainer, DetailsLogoTitle, DetailsNoCommentsMessage, DetailsRightContainer } from "./style"
+import geminiAPI from "../../services/geminiAPI"
+import { useParams } from "react-router-dom"
 
 function Details () {
+  const { title, author } = useParams()
+  const [summary, setSummary] = useState("")
+
+  async function generateSummary() {
+    const response = await geminiAPI.post(`v1beta/models/gemini-1.5-flash:generateContent`, {
+      "generationConfig":{},
+      "safetySettings":[],
+      "contents":[
+        {
+          "role":"user",
+          "parts":[
+            {
+              text:`Gere um resumo, em português brasileiro, sobre o livro ${title}, do autor ${author}`
+            }
+          ]
+        }
+      ]
+    })
+    setSummary(response.data.candidates[0].content.parts[0].text)
+    console.log(response.data.candidates[0].content.parts[0].text)
+  }
+  useEffect(() => {
+    generateSummary()
+  },[])
   return (
     <DetailsContainer>
       <Logo>
@@ -12,7 +39,7 @@ function Details () {
       <DetailsBottomContainer>
         <DetailsLeftContainer>
           <DetailsContentContainer>
-          <p>Lorem ipsum dolor sit amet.r sit amet.sdfsadfs asfasrase serwrwe4 eraserwe we  werwerr. Lorem ipsum dolor sit amet.r sit amet.sdfsadfs asfasrase serwrwe4 eraserwe we  werwerr. Lorem ipsum dolor sit amet.r sit amet.sdfsadfs asfasrase serwrwe4 eraserwe we  werwerr. Lorem ipsum dolor sit amet.r sit amet.sdfsadfs asfasrase serwrwe4 eraserwe we  werwerr. Lorem ipsum dolor sit amet.r sit amet.sdfsadfs asfasrase serwrwe4 eraserwe we  werwerr. </p>
+          <p>{summary}</p>
           </DetailsContentContainer>
         </DetailsLeftContainer>
         <DetailsRightContainer>
