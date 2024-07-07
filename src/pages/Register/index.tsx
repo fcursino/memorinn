962cce49e79ee8e93e240778c1f9ccfc315555a4
fcursino <fabriciocursino09@gmail.com>
2024-07-07 16:12:37 -0300

    implementing frontend integration with user register route

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react"
 import Input from "../../components/Input"
 import Logo from "../../components/Logo"
 import { RegisterButton, RegisterContainer, RegisterLogoTitle, RegisterWarning } from "./style"
-import { useLocation } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom"
+import memorinnAPI from "../../services/memorinnAPI"
 
 function Register () {
   const [name, setName] = useState("")
@@ -11,7 +12,9 @@ function Register () {
   const [password, setPassword] = useState("")
   const [registerEnabled, setRegisterEnabled] = useState(true)
   const [validated, setValidated] = useState(false)
+  const [registered, setRegistered] = useState(false)
   const query = new URLSearchParams(useLocation().search)
+  const navigate = useNavigate()
 
   useEffect(() => {
     const emailFromLogin = query.get('email')
@@ -20,15 +23,33 @@ function Register () {
     }
   }, [])
 
-  function handleRegister () {
+  async function handleRegister () {
+    setRegistered(false)
     setRegisterEnabled(false)
     try {
       setValidated(false)
       setValidated(true)
+      if(name && userName && email && password) {
+        const response = await registerNewUser()
+        if(response.data) {
+          navigate('/')
+          return false
+        } 
+        setRegistered(true)
+      }
       setRegisterEnabled(true)
     } catch (error) {
       console.log(error)
     }
+  }
+
+  async function registerNewUser(): Promise<any> {
+    return await memorinnAPI.post('/users', {
+      email: email,
+      password: password,
+      name: name,
+      userName: userName
+    })
   }
   
   return (
@@ -45,6 +66,7 @@ function Register () {
       <Input placeholder="senha" value={password} type="password" changeSearch={setPassword} />
       <RegisterWarning>{!password.trim() && validated ? "Senha obrigatória" : null}</RegisterWarning>
       <RegisterButton onClick={handleRegister} disabled={!registerEnabled}>Cadastrar</RegisterButton>
+      <RegisterWarning>{registered && validated ? "Erro ao fazer cadastro. Verifique suas credenciais e tente novamente" : null}</RegisterWarning>
     </RegisterContainer>
   )
 }
