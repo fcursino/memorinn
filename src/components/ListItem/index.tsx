dae0901a2c1d1fcba73b83c0ba873f518199a9e1
fcursino <fabriciocursino09@gmail.com>
2024-07-09 19:26:53 -0300

    adding function as a ListItem component param to receive onclick

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -3,11 +3,12 @@ import { ListItemContainer } from "./style";
 interface ListItemProps {
   children?: React.ReactNode
   type: string
+  onClick?: () => void
 }
 
-const ListItem: React.FC<ListItemProps> = ({type, children}) => {
+const ListItem: React.FC<ListItemProps> = ({type, children, onClick}) => {
   return (
-    <ListItemContainer type={type}>
+    <ListItemContainer type={type} onClick={onClick}>
       {children}
     </ListItemContainer>
   )
