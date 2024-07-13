3023a39861a03638ecbcf48617f945205cfaff90
fcursino <fabriciocursino09@gmail.com>
2024-07-13 19:13:25 -0300

    including dev env base url into memorinn service

--- src/services/memorinnAPI/index.ts
+++ src/services/memorinnAPI/index.ts
@@ -2,5 +2,6 @@ import axios from 'axios'
 
 const memorinnAPI = axios.create({
   baseURL: 'https://memorinn-webservice.vercel.app/',
+  // baseURL: 'http://localhost:3333/'
 });
 export default memorinnAPI
