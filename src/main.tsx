1ea31f33749ac7c8d0a7de6a0201adefdeed2bd7
fcursino <fabriciocursino09@gmail.com>
2024-07-25 20:57:48 -0300

    implementing redux provider around app

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import { AuthProvider } from './hooks/AuthContext.tsx'
 import App from './App.tsx'
+import { Provider } from 'react-redux'
+import { store } from './redux/store.ts'
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <Provider store={store}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Provider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 )
