06b28d322bc599f2680fb8337061dfd70db41ec2
fcursino <fabriciocursino09@gmail.com>
2024-07-30 21:11:50 -0300

    implementing login validation inside Navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,13 +1,31 @@
-import { useEffect } from "react";
 import { NavbarContainer } from "./style";
-import { store } from "../../state/store";
+import { AppDispatch, store } from "../../state/store";
+import memorinnAPI from "../../services/memorinnAPI";
+import Cookies from "js-cookie"
+import { login } from "../../state/auth/authSlice";
+import { useDispatch } from "react-redux";
 
-const Navbar = () => {
+const Navbar = async () => {
+    const dispatch = useDispatch<AppDispatch>()
     const { auth } = store.getState()
-    useEffect(() => {
-        const user = auth.value
-        if(!user) {}
-    }, [])
+    const user = auth.value
+        if(!user) {
+            const token = Cookies.get("token")
+            if(token) {
+                const response = await memorinnAPI.post(`/users/login/${token}`)
+                if(response.data) {
+                    const loggedUser = response.data.user
+                    dispatch(login({
+                      token: response.data.token,
+                      email: loggedUser.email,
+                      id: loggedUser.id,
+                      userName: loggedUser.userName,
+                      name: loggedUser.name
+                    }))
+                  }
+            }
+            
+        }
     return (
         <NavbarContainer/>
     )
