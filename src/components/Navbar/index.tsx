579ac8421c01258a53ab13c37d49db9510895eec
fcursino <fabriciocursino09@gmail.com>
2024-08-01 21:16:28 -0300

    implementing use of login route to refresh user auth

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,33 +1,45 @@
+import { useEffect } from "react";
 import { NavbarContainer } from "./style";
-import { AppDispatch, store } from "../../state/store";
+import { useDispatch, useSelector } from "react-redux";
 import memorinnAPI from "../../services/memorinnAPI";
-import Cookies from "js-cookie"
+import Cookies from "js-cookie";
 import { login } from "../../state/auth/authSlice";
-import { useDispatch } from "react-redux";
+import { RootState } from "../../state/store";
 
-const Navbar = async () => {
-    const dispatch = useDispatch<AppDispatch>()
-    const { auth } = store.getState()
-    const user = auth.value
-        if(!user) {
-            const token = Cookies.get("token")
-            if(token) {
-                const response = await memorinnAPI.post(`/users/login/${token}`)
-                if(response.data) {
-                    const loggedUser = response.data.user
-                    dispatch(login({
-                      token: response.data.token,
-                      email: loggedUser.email,
-                      id: loggedUser.id,
-                      userName: loggedUser.userName,
-                      name: loggedUser.name
-                    }))
-                  }
+const Navbar = () => {
+    const dispatch = useDispatch();
+    const user = useSelector((state: RootState) => state.auth.value);
+
+    useEffect(() => {
+        const checkAndLogin = async () => {
+            if (!user.token) {
+                const token = Cookies.get("token");
+                console.log(token)
+                if (token) {
+                    try {
+                        const response = await memorinnAPI.post(`/users/login/${token}`);
+                        if (response.data) {
+                            const loggedUser = response.data.user;
+                            dispatch(login({
+                                token: response.data.token,
+                                email: loggedUser.email,
+                                id: loggedUser.id,
+                                userName: loggedUser.userName,
+                                name: loggedUser.name
+                            }));
+                        }
+                    } catch (error) {
+                        console.error("Login failed:", error);
+                    }
+                }
             }
-            
-        }
+        };
+        checkAndLogin();
+    }, []);
+
     return (
-        <NavbarContainer/>
-    )
+        <NavbarContainer />
+    );
 }
-export default Navbar
+
+export default Navbar;
